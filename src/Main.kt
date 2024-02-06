75f267a88a9d3de37acd76abb70a91bab806753b
joshlasota <142518176+joshlasota@users.noreply.github.com>
2024-02-06 10:44:19 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val supplies = arrayOf("Hot dogs", "Buns", "Ketchup", "Mustard", "Napkins")
+
+    println("Supplies needed:")
+    for (supply in supplies) {
+        println("- $supply")
+    }
+}
